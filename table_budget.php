e2a509bcb9500c566963dbec44e4deed7503bb5f
toeylove2012 <89264241+toeylove2012@users.noreply.github.com>
2022-09-19 18:02:15 +0700

    Update table_budget.php

--- table_budget.php
+++ table_budget.php
@@ -104,7 +104,7 @@ th.rotated-text>div>span {
       </li>
       <li class="dropdown user user-menu open">
       <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">
-      <img src="img/profile.png" alt="Avatar" class="avatar">
+      <img src="imgs/profile.png" alt="Avatar" class="avatar">
       <span class="hidden-xs"><?php echo $_SESSION['firstname'] . ' ' . $_SESSION['lastname'] ?></span>
           </a>
           <div class="dropdown-menu dropdown-menu dropdown-menu-right">
@@ -125,7 +125,7 @@ th.rotated-text>div>span {
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <div class="brand-link">
-      <img src="img/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="imgs/logo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">BNCC Budgetplan</span>
 </div>
 
